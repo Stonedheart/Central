a38a362e5975db2992b4f9f623a26292ac217778
Stonedheart <tytomaszto@gmail.com>
2016-10-31 00:52:21 +0100

    Add files via upload
    Final version, easy to read and understand

--- 100doors.py
+++ 100doors.py
@@ -3,19 +3,11 @@ DOOR_CLOSED = False
 DOORS_COUNT = 4
 
 """List initialization"""
-
-#original:
-#for i in range(1, DOORS_COUNT+1):
-#    doors.append(DOOR_CLOSED)
-
-#shorter version, working, correct?
 doors = [DOOR_CLOSED for DOOR_CLOSED in range(1, DOORS_COUNT+1)]
 
 
 """Walkthrough"""
 print(doors) #not really necessary
-
-#original
 for walk in range(1, DOORS_COUNT+1):
     for index, door in enumerate(doors):
         if (index+1) % walk == 0:
@@ -23,17 +15,13 @@ for walk in range(1, DOORS_COUNT+1):
                 doors[index] = False
             else:
                 doors[index] = True
-
     print(doors)
 
 
 """Final display"""
 opened_doors = ""
-
-#original
 for index, door in enumerate(doors):
         if door == DOOR_CLOSED:
             opened_doors = opened_doors + ', ' + str(index+1)
-
 
 print("This doors are open: " + opened_doors)
